3f66499a6f4bbc2243957b6481f4eefeeee1c943
João Martinho <jdmartinho@gmail.com>
2017-11-10 18:49:00 +0000

    Added share buttons.

--- js/util.js
+++ js/util.js
@@ -1,14 +1,41 @@
-Number.prototype.formatMoney = function(c){
-var n = this,
+Number.prototype.formatMoney = function(c) {
+  var n = this,
     c = isNaN(c = Math.abs(c)) ? 2 : c,
     d = ",",
     t = ".",
     s = n < 0 ? "-" : "",
     i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
     j = (j = i.length) > 3 ? j % 3 : 0;
-   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
- };
+  return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
 
- $(document).ready(function(){
-     $('[data-toggle="tooltip"]').tooltip();
- });
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Uses sharer.js
+  // https://ellisonleao.github.io/sharer.js/#twitter
+  var url = "http://calcirs.pt";
+  var subject = "CalcIRS - Calcule o seu IRS online";
+  console.log(url);
+  console.log(title);
+
+  // Facebook
+  $('#share-fb').attr('data-url', url).attr('data-sharer', 'facebook');
+  // Twitter
+  $('#share-tw').attr('data-url', url).attr('data-title', subject).attr('data-sharer', 'twitter');
+  // WhatsApp
+  $('#share-wa').attr('data-url', url).attr('data-title', subject);
+  // Linkedin
+  $('#share-li').attr('data-url', url).attr('data-sharer', 'linkedin');
+  // Google plus
+  $('#share-gp').attr('data-url', url).attr('data-title', title).attr('data-sharer', 'googleplus');
+  // Email
+  $('#share-em').attr('data-url', url).attr('data-title', title).attr('data-subject', subject).attr('data-sharer', 'email');
+
+  // Prevent basic click behavior
+  $(".sharer button").click(function() {
+    event.preventDefault();
+  });
+});
